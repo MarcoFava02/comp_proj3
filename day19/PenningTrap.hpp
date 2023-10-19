e6c5782e602397c932980062309bce0686a1aa05
alejandrortegaa <146114428+alejandrortegaa@users.noreply.github.com>
2023-10-19 18:49:30 +0200

    Add files via upload

---
+++ day19/PenningTrap.hpp
@@ -0,0 +1,68 @@
+
+
+
+#ifndef __PenningTrap_hpp__   
+#define __PenningTrap_hpp__
+
+
+#include <stdio.h>
+#include <armadillo>
+#include <math.h>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "Particle.hpp"
+
+class PenningTrap
+{
+    public:
+        double B0_, V0_, d_;
+        std::vector <Particle> Particle_;
+    
+
+        //Constructor
+        PenningTrap(double B0, double V0, double d);
+
+        // Add a particle to the trap
+        void add_particle(Particle p_in);
+
+        // External electric field at point r=(x,y,z)
+        arma::vec external_E_field(arma::vec r, double t);  
+
+        // External magnetic field at point r=(x,y,z)
+        arma::vec external_B_field(arma::vec r);  
+
+        // Force on particle_i from particle_j
+        arma::vec force_particle(int i, int j);
+
+        // The total force on particle_i from the external fields
+        arma::vec total_force_external(int i, double t);
+
+        // The total force on particle_i from the other particles
+        arma::vec total_force_particles(int i);
+
+        // The total force on particle_i from both external fields and other particles
+        arma::vec total_force(int i, double t);
+
+        // Evolve the system one time step (dt) using Runge-Kutta 4th order
+        void evolve_RK4(double dt, double t);
+
+        // Evolve the system one time step (dt) using Forward Euler
+        void evolve_forward_Euler(double dt, double t);
+
+        // The total force on particle_i from the other particles
+        arma::vec analytical_func(Particle p_in, double t);
+
+
+
+}; 
+
+
+
+
+    
+#endif /* PenningTrap_hpp */
---
+++ day19/mainTrap.cpp
@@ -0,0 +1,87 @@
+
+#include "SourceFile.hpp"
+
+int main(){
+
+      
+    const double V0=2.41e+6;
+    const double B0=9.65e+1;
+    const double d=500.;
+
+    
+
+    
+    //creating particle 1
+    double q1 = 1.; //units of e
+    double m1 = 40.078; //units u
+    arma::vec r01 = arma::vec{20., 0., 20.}; //units mu m
+    arma::vec v01 = {0., 25., 0.}; //units mu m / mu s
+
+    Particle particle1(q1,m1,r01,v01);
+
+
+    //creating particle 2
+    double q2 = 1.; //units of e
+    double m2 = 40.078; //units u
+    arma::vec r02 = arma::vec{25., 25., 0.}; //units mu m
+    arma::vec v02 = {0., 40., 5.}; //units mu m / mu s
+
+    Particle particle2(q2,m2,r02,v02);
+
+
+    PenningTrap trap(B0, V0, d);
+
+    
+    //insert particle1 and particle2 in the vector Particle_ inside trap
+    trap.add_particle(particle1);       // std::cout << trap.Particle_[0].q_ << "\n";
+    trap.add_particle(particle2);
+
+    
+    
+
+    //compute the force between particle_i and particle_j
+    for(int i = 0; i<trap.Particle_.size(); i++)
+    {
+        for(int j = 0; j<trap.Particle_.size(); j++)
+        {
+            arma::vec force = trap.force_particle(i,j);
+            std::cout << "Force received by particle " << i << " caused by particle " << j << ": \n" << force <<std::endl;
+        }
+    }
+
+    float t_tot = 50.;
+    int n_step = 4000;
+    float dt = t_tot/n_step;
+    float t=0;
+
+    
+
+    for (int i=0; i<n_step; i++){
+        trap.evolve_RK4(dt,t);
+        
+        std::cout << trap.Particle_[1].r_ << std::endl;
+        //We could have also use the method Forward Euler instead:
+            //PenningTrap1.evolve_forward_Euler(dt);
+        
+        //Update the actual time of the system and printing it with the position z of the particle
+        t+=dt;
+    }
+
+
+    
+
+    //computing E and B for a certain r --omit this
+    arma::vec r = arma::vec{1,0,0};
+    arma::vec E = trap.external_E_field(r, t);     //std::cout << "E: " << E << "\n";
+    arma::vec B = trap.external_B_field(r);     //std::cout << "B: " << B << "\n";    
+
+
+
+    return 0;
+}
+
+// std::cout << trap.B0 << "\n" << trap.V0 << "\n" << trap.d << std::endl;
+
+    // std::cout << "B0: " << trap.B0 << "\n";
+    // std::cout << "V0: " << trap.V0 << "\n";
+    // std::cout << "d: " << trap.d << "\n";
